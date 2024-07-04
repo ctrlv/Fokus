c13c587005153f0b658d5cb1cf44d860ef400a0b
Matheus Medina <matheusmvo@hotmail.com>
2024-07-04 14:27:13 -0300

    Projeto fokus V2

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,21 @@
 const addTarefaBt = document.querySelector('.app__button--add-task')
 const formAddTarefa = document.querySelector('.app__form-add-task')
 const textArea = document.querySelector('.app__form-textarea')
+const ulTarefas = document.querySelector('.app__section-task-list')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
-const tarefas = []
+const btnRemoverConcluidas = document.querySelector('#btn-remover-concluidas')
+const btnRemoveTodas = document.querySelector('#btn-remover-todas')
+
+let tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+let tarefaSelecionada = null
+let liTarefaSelecionada = null
+
+
+
+function attTarefas () {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li')
@@ -10,18 +23,30 @@ function criarElementoTarefa(tarefa) {
 
     const svg = document.createElement('svg')
     svg.innerHTML = `
-    <svg>
         <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
 
     const paragrafo = document.createElement('p')
     paragrafo.textContent = tarefa.descricao
+    paragrafo.classList.add('app__section-task-list-item-description')
 
     const botao = document.createElement('button')
+    botao.classList.add('app_button-edit')
+    
+    botao.onclick = () => {
+        const novaDescricao = prompt("Qual o novo nome da tarefa?")
+        if (novaDescricao) {
+            paragrafo.textContent = novaDescricao
+        tarefa.descricao = novaDescricao
+        attTarefas()
+        }
+        
+    }
+
+
     const imagemBt = document.createElement('img')
     imagemBt.setAttribute('src' , './imagens/edit.png' )
 
@@ -29,6 +54,37 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botao)
+
+
+    if (tarefa.completa) {
+        li.classList.add('app__section-task-list-item-complete')
+        botao.setAttribute('disabled', 'disabled')
+    }else{
+           li.onclick = () =>{
+
+          document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active')
+            })
+            
+        if(tarefaSelecionada == tarefa){
+            paragrafoDescricaoTarefa.textContent = ''
+            tarefaSelecionada = null
+            liTarefaSelecionada = null
+            return
+
+        }
+            tarefaSelecionada = tarefa
+            liTarefaSelecionada = li
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+      
+        li.classList.add('app__section-task-list-item-active')
+    }
+    }
+
+ 
+
+    return li
     
 }
 
@@ -43,5 +99,42 @@ formAddTarefa.addEventListener('submit', (evento) => {
         descricao: textArea.value
     }
     tarefas.push(tarefa)
-    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+    attTarefas()
+    textArea.value = ''
+    formAddTarefa.classList.add('hidden')
 })
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa)
+    ulTarefas.append(elementoTarefa)
+});
+
+document.addEventListener('focoFinalizado', () =>{
+    if(tarefaSelecionada && liTarefaSelecionada){
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+        tarefaSelecionada.completa = true
+        attTarefas()
+    }
+
+})
+
+
+const removeTarefas = (somenteCompletas) =>{
+    //const seletor = somenteCompletas ? ".app__section-task-list-item-complete" : ".app__section-task-list-item"
+    let seletor = ".app__section-task-list-item"
+    if (somenteCompletas) {
+        seletor = ".app__section-task-list-item-complete"
+    }
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    })
+    tarefas = somenteCompletas ? tarefas.filter(tarefa => !tarefa.completa) : []
+    attTarefas()
+}
+
+btnRemoverConcluidas.onclick = () => removeTarefas(true)
+btnRemoveTodas.onclick = () => removeTarefas(false)
